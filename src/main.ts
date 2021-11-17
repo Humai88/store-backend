4fb47b22a22850e4e0366590eed1a12f0e612b3f
Gumay <humaivier88@gmail.com>
2021-11-17 18:18:51 +0000

    Add env var

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server";
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+let port = process.env.PORT || 4000;
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
